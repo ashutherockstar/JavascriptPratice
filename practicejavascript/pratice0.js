f692956592eea4df92e690baa39a058e1a34e960
ashutherockstar <ashutoshranjansingh458@gmail.com>
2022-03-07 02:14:59 +0530

    2nd Commit

--- practicejavascript/pratice0.js
+++ practicejavascript/pratice0.js
@@ -2,9 +2,9 @@ console.time('Your code took')
 console.log("Hello Console");
 console.log(5*5);
 console.log({name:"Ashutosh", age :"25"});
-console.table({name:"Ashutosh", age :"25"});
-console.warn('This is a warning');
-console.clear();
+// console.table({name:"Ashutosh", age :"25"});
+// console.warn('This is a warning');
+// console.clear();
 /*
 console.timeEnd('Your code took');
 console.assert(99<88,'This is err');
